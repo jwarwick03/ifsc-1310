f3722135cfdc9aa0f9bc0d581819f059a5a1ad14
Joel Warwick <60834182+jwarwick03@users.noreply.github.com>
2023-12-06 18:34:29 +0000

    jquery slides

--- spacex-final/assets/js/scripts.js
+++ spacex-final/assets/js/scripts.js
@@ -11,5 +11,24 @@ $(document).ready(function() {
       autoplaySpeed: 2000
     });
 
+  $(".click-toggle").click(function() {
+    $(".drop-down").slideDown();
+    $(this).css({ "transform": "rotate(180deg)"
+    });
+  });
+
+
+// Cards Read More //
+  $("#rm-1").click(function() {
+    $("#content-1").toggle(500);
+  });
+
+  $("#rm-2").click(function() {
+    $("#content-2").toggle(500);
+  });
+
+  $("#rm-3").click(function() {
+    $("#content-3").toggle(500);
+  });
 
 });
---
+++ spacex-v1/assets/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".faq-title").click(function() {
+        $(".slide-content")slideToggle();
+    });
+});
+
